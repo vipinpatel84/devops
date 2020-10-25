5383fc19a34d7693bfa4630fb49c8504cc035724
Vipin Patel <vipin.vipinpatel84@gmail.com>
2020-10-25 20:25:41 +0530

    initial commit

---
+++ devops/src/main/java/com/devops/devops/DevopsApplication.java
@@ -0,0 +1,14 @@
+package com.devops.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(DevopsApplication.class, args);
+	}
+
+}
