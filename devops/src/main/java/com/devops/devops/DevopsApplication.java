14dac7bbeac3446ebd41d36b461b6749b1c88bfd
Vipin Patel <vipin.vipinpatel84@gmail.com>
2020-10-25 20:46:11 +0530

    git plugin added to Eclipse

--- devops/src/main/java/com/devops/devops/DevopsApplication.java
+++ devops/src/main/java/com/devops/devops/DevopsApplication.java
@@ -8,6 +8,7 @@ public class DevopsApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("git plugin added");
 		SpringApplication.run(DevopsApplication.class, args);
 	}
 
